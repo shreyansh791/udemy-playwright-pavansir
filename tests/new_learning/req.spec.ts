c9e3d66eaa67f85a923007a2d140343f2e9571d4
Shreyansh Jain <shreyansh791@yahoo.in>
2025-08-27 18:21:09 +0530

    script to find failed request

--- tests/new_learning/req.spec.ts
+++ tests/new_learning/req.spec.ts
@@ -13,3 +13,16 @@ test("Check for failed requests", async ({ page }) => {
 
   await page.waitForTimeout(50); // without this the test exists without error
 });
+
+test("check for errors inside the console", async ({ page }) => {
+  page.on("console", (message) => {
+    expect
+      .soft(message.type(), `Received error log: ${message.text()}`)
+      .not.toEqual("error");
+  });
+
+  await page.goto("http://localhost:5000/Events.html");
+  const requestButton = page.getByRole("button", { name: "Call wrong server" });
+  await requestButton.click();
+  await page.waitForTimeout(50); // ensures console log processing
+});
